00deeed5ed17ac795cce702fc419c78ef4b98f48
super-delicious <yixin.cao@sjsu.edu>
2020-10-15 12:56:36 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(889, 500);
+  background(220, 22, 220);
+
+  strokeWeight(3);
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed == true) {
+    stroke(map(mouseX, 0, 600, 0, 255, true),)
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(width - mouseX, height - mouseY, pmouseX, pmouseY);
+  }
+}
+
+function keyTyped() {
+  //  console.log(`key ${key} is being typed`)
+  if (key === 's'){
+  // save this image
+  saveCanvas('filename', 'png');
+  }
 }
