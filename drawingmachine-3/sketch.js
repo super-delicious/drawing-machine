1032e2341e4ca9a8a92e3288a2d9d7116057d896
super-delicious <yixin.cao@sjsu.edu>
2020-10-22 12:15:10 -0700

    update

--- drawingmachine-3/sketch.js
+++ drawingmachine-3/sketch.js
@@ -5,12 +5,15 @@ let slider;
 let r = 100;
 let g = 100;
 let b = 100;
+let noiseX = 0.0;
+let strokeWidth = 5;
 
 function setup() {
+  //createCanvas(889, 500);
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  background(255, 32, 54);
 
-  strokeWeight(r, g, b);
+//  strokeWeight(r, g, b);
 
   button1 = createButton("stroke weight");
   button2 = createButton("stroke color");
@@ -22,15 +25,20 @@ function setup() {
 }
 
 function draw() {
-  let lineWidth = slider.value();
+  background(255, 32, 54, 20);
+  strokeWeight(strokeWidth);
 
-  strokeWeight(lineWidth);
-  background(100);
+  noiseOffset += 0.01;
+ strokeWidth = noise(noiseOffset) * 20;
+
+  let lineWidth = slider.value();
   if (keyIsPressed === true) {
-    //stroke(map(mouseX, 0, 600, 0, 255, true), )
-    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
     line(width - mouseX, height - mouseY, pmouseX, pmouseY);
-    array.push([mouseX, mouseY]);
+
+
+    //array.push([mouseX, mouseY]);
   }
 }
 
@@ -43,28 +51,28 @@ function keyTyped() {
     //display imgae
     //console.log(array[0]);
     //console.log(array[0][1]);
-    background(5);
+    background(255);
     beginShape();
     for (let i = 0; i < array.length; i++) {
       //line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-    curveVertex(array[i][0], array[i][1]);
+      curveVertex(array[i][0], array[i][1]);
     }
     endShape();
-  }else if (key === 'c') {
-      // clear canvas
-      color(0, 0, 0);
+  } else if (key === 'c') {
+    // clear the canvas
+    clear();
 
-    } else if (key === 'r') {
-      r += 5;
-    } else if (key === 'g') {
-      g += 5;
-    } else if (key === 'b') {
-      b += 5;
-  }   else if (key === 't') {
-        r -= 5;
-      } else if (key === 'h') {
-        g -= 5;
-      } else if (key === 'n') {
-      b -= 5;
-    }
+  } else if (key === 'r') {  // change color value
+    r += 5;
+  } else if (key === 'g') {
+    g += 5;
+  } else if (key === 'b') {
+    b += 5;
+  } else if (key === 't') {
+    r -= 5;
+  } else if (key === 'h') {
+    g -= 5;
+  } else if (key === 'n') {
+    b -= 5;
+  }
 }
